3281133e5393bbf39e65daf07db14bccf3a05cd3
Kirk McGlamery <kirkm72@att.net>
2019-12-30 17:12:17 -0600

    updated APIroutes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,48 +1,21 @@
 let friends = require("../data/friends"); //Possible matches JSON
 
 module.exports = function(app) {
-  // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
-
-  app.get("/api/friends", function(req, res) {
+ app.get("/api/friends", function(req, res) {
     res.json(friends);
   });
 
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // ---------------------------------------------------------------------------
-
   app.post("/api/friends", function(req, res) {
-    // Note the code here. Our "server" will respond to a user"s survey result
-    // Then compare those results against every user in the database.
-    // It will then calculate the difference between each of the numbers and the user"s numbers.
-    // It will then choose the user with the least differences as the "best friend match."
-    // In the case of multiple users with the same result it will choose the first match.
-    // After the test, it will push the user to the database.
-
-    // We will use this object to hold the "best match". We will constantly update it as we
-    // loop through all of the options
     var bestMatch = {
       name: "",
       photo: "",
       friendDifference: Infinity
     };
-
-    // Here we take the result of the user"s survey POST and parse it.
-    var userData = req.body;
+    var userData = req.body; //parse user entries & compare to database
     var userScores = userData.scores;
-
-    // This variable will calculate the difference between the user"s scores and the scores of
-    // each user in the database
     var totalDifference;
 
-    // Here we loop through all the friend possibilities in the database.
-    for (var i = 0; i < friends.length; i++) {
+    for (var i = 0; i < friends.length; i++) { // loop through JSON array
       var currentFriend = friends[i];
       totalDifference = 0;
 
@@ -65,12 +38,7 @@ module.exports = function(app) {
         bestMatch.friendDifference = totalDifference;
       }
     }
-
-    // Finally save the user's data to the database (this has to happen AFTER the check. otherwise,
-    // the database will always return that the user is the user's best friend).
-    friends.push(userData);
-
-    // Return a JSON with the user's bestMatch. This will be used by the HTML in the next page
-    res.json(bestMatch);
+    friends.push(userData); //push to database
+    res.json(bestMatch); //return best match JSON
   });
 };
