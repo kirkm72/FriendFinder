2041f66e7f4a037da1133c9cdc7a6a0657a1b46e
Kirk McGlamery <kirkm72@att.net>
2020-01-05 13:31:35 -0600

    cleaned up modal

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 let express = require("express"); // Using Node module Express
 let app = express(); // Referencing express as "app"
-var PORT = process.env.PORT || 3000; //Setting port to be preferred to be set to 8080
-app.use(express.urlencoded({ extended: true })); // Add ability to read URLencoded payloads 
-app.use(express.json()); // Add ability to read JSON payloads
+var PORT = process.env.PORT || 3000; //Set default port to 3000
+app.use(express.urlencoded({ extended: true })); // Add ability to read URLencoded
+app.use(express.json()); // Add ability to read JSON
 require("./app/routing/apiRoutes")(app);  //Using our api(JSON) routes
 require("./app/routing/htmlRoutes")(app); //Using our HTML routes
 
